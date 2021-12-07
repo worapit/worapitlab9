4a053293afc549218b37bee8bbd0dc8eb7662d28
worapit <drm.wmwork@gmail.com>
2021-12-07 15:58:57 +0700

    complete

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,11 +1,41 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your property: "
-"Your status = "
-"UNFRIEND"
-"FRIEND"
-"BEST FRIEND"
-"ONE-NIGHT-STAND"
-"MARRIED"
-*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age ,height,money ;
+    cout << "Enter your age: ";
+    cin >> age ;
+
+    if(age <= 20){
+        cout << "Enter your height: ";
+        cin >> height;
+        if(height < 160){
+            cout << "Your status = UNFRIEND";
+        }else if(height <175){
+             cout << "Your status = FRIEND" ;
+        }else{
+            cout << "Enter your property: ";
+            cin >> money; 
+            if(money > 69000000){
+                cout << "Your status = MARRIED" ;
+            }else{
+                cout << "Your status = ONE-NIGHT-STAND";
+            }
+        }
+
+
+    }else if(age < 30){
+        cout << "Enter your property: ";
+        cin >> money; 
+        if(money > 10000000){
+                cout << "Your status = BEST FRIEND";      
+            }else{
+                cout << "Your status = UNFRIEND" ;
+            }
+        }else{
+        cout << "Your status = UNFRIEND" ;
+        }
+}
+
+
+
